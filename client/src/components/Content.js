c7e8bcb335cdcb1533259ff222a1bdea06d54b72
lermand7 <xarma007@gmail.com>
2021-06-10 18:58:09 +0200

    Update Content.js

--- client/src/components/Content.js
+++ client/src/components/Content.js
@@ -11,9 +11,10 @@ function Content()
     return (
         <>
             <div id="home" className="video-div">
+            dangerouslySetInnerHTML={{ __html: `
                 <video playsInline autoPlay={true} loop muted className="home-video">
                     <source src={particles} type="video/mp4" />
-                </video>
+                </video>` }}
                 <div className="home-div">
                     <div className="home-div-center">
                         <h1>ÜDVÖZÖLLEK, UTAZÓ!</h1>
